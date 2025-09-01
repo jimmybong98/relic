ee0efc41246e0d11726ab6198eb26f9b0061288e
jimmybong98 <167213072+jimmybong98@users.noreply.github.com>
2025-09-01 07:42:27 -0300

    Update main_menu_page.dart

--- lib/features/main_menu/main_menu_page.dart
+++ lib/features/main_menu/main_menu_page.dart
@@ -27,25 +27,25 @@ class MainMenuPage extends StatelessWidget {
           const SizedBox(height: 32),
           Expanded(
             child: GridView.count(
-              crossAxisCount: 2,
-              crossAxisSpacing: 16,
-              mainAxisSpacing: 16,
-              padding: const EdgeInsets.all(16),
+              crossAxisCount: 4,
+              crossAxisSpacing: 8,
+              mainAxisSpacing: 8,
+              padding: const EdgeInsets.all(8),
               children: [
                 _MenuCard(
-                  title: 'Preparador',
+                  title: 'Liberação de máquina (FOR007)',
                   onTap: () => open(context, const PreparacaoPage()),
                 ),
                 _MenuCard(
-                  title: 'Operador',
+                  title: 'Amostragem (FOR009)',
                   onTap: () => open(context, const OperadorPage()),
                 ),
                 _MenuCard(
-                  title: 'Finalizar OS',
+                  title: 'Finalização de OS (FOR008)',
                   onTap: () => open(context, const FinalizarOsPage()),
                 ),
                 _MenuCard(
-                  title: 'Supervisão',
+                  title: 'Administração',
                   onTap: () => open(context, MainScreen()),
                 ),
               ],
