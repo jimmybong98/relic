3eef90098e6cf05cc33db8488379f17863b38d69
jimmybong98 <henriquedoor@gmail.com>
2025-09-29 15:44:45 -0300

    Zika

--- lib/constants.dart
+++ lib/constants.dart
@@ -5,5 +5,5 @@ const secondaryColor = Color(0xFF2A2D3E);
 const accentColor = Color(0xFF4DD0E1);
 const surfaceColor = Color(0xFF1F212A);
 const surfaceVariantColor = Color(0xFF2B2D36);
-const bgColor = Color(0xFF14151C);
+const bgColor = Color(0xFF303030);
 const defaultPadding = 16.0;
